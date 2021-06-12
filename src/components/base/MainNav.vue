939cacbafbcf3166666d7062c103165f8f11a87f
flkt <flkt.crnpio@gmail.com>
2021-06-11 19:21:27 -0500

    cambio margen menu alineado con header y footer

--- src/components/base/MainNav.vue
+++ src/components/base/MainNav.vue
@@ -1,25 +1,29 @@
 <template>
   <nav v-if="hasMainNav" class="main-nav">
-    <div class="main-nav-top">
-      <BtnNavMob @click.native="toggleMenu" :class="{'open':isOpenMainNav}" />
-    </div>
-    <div class="main-nav-menu" :class="{'open':isOpenMainNav}" @click="toggleMenu">
-      <a class="main-nav-external-link" href="#" target="_blank" rel="noopener">
-        Ir a ENI (nombre)
-      </a>
-      <router-link class="main-nav-link" to="/">Inicio</router-link>
-      <router-link class="main-nav-link" to="/guia-estilos">Guia de estilos</router-link>
-      <router-link class="main-nav-link" to="/ayuda">Ayuda</router-link>
-    </div>
+    <MainContainer class="flex">
+      <div class="main-nav-top">
+        <BtnNavMob @click.native="toggleMenu" :class="{'open':isOpenMainNav}" />
+      </div>
+      <div class="main-nav-menu" :class="{'open':isOpenMainNav}" @click="toggleMenu">
+        <a class="main-nav-external-link" href="#" target="_blank" rel="noopener">
+          Ir a ENI (nombre)
+        </a>
+        <router-link class="main-nav-link" to="/">Inicio</router-link>
+        <router-link class="main-nav-link" to="/guia-estilos">Guia de estilos</router-link>
+        <router-link class="main-nav-link" to="/ayuda">Ayuda</router-link>
+      </div>
+    </MainContainer>
   </nav>
 </template>
 
 <script>
+import MainContainer from '@/components/base/MainContainer.vue';
 import BtnNavMob from '@/components/base/BtnNavMob.vue';
 
 export default {
   name: 'MainNav',
   components: {
+    MainContainer,
     BtnNavMob,
   },
   methods: {
@@ -41,21 +45,19 @@ export default {
 <style scoped lang="scss">
 .main-nav {
   background: map-get($color-gob, "verde");
-  position: relative;
-  // position: sticky;
-  // top: 50px;
-  // z-index: 9998;
+  box-shadow: 0 0 10px rgba(0,0,0,.7);
 }
 .main-nav-top {
   background: map-get($color-gob, "verde");
   position: relative;
+  margin-right: - ($gap);
+  margin-left: - ($gap);
   z-index: 2;
   box-shadow: 0 0 20px rgba(0,0,0,.2);
 }
 .main-nav-menu {
   background: map-get($color-gob, "verde");
   border-top: 1px solid rgba(255,255,255,.2);
-  box-shadow: 0 0 10px rgba(0,0,0,.7);
   display: flex;
   flex-direction: column;
   overflow-y: auto;
@@ -64,15 +66,20 @@ export default {
   height: calc(100vh - 100px);
   height: calc((100 * var(--vh)) - 100px);
   width: 100%;
+  margin-right: - ($gap);
+  margin-left: - ($gap);
   transition: top .3s ease-in-out;
   z-index: 1;
   &.open {
     top: 50px;
   }
   @media (min-width: map-get($media-queries-limit, "navegacion")) {
+    position: relative;
     flex-direction: row;
     height: auto;
     top: 0px;
+    margin-right: - ($gap);
+    margin-left: - ($gap);
     &.open {
       top: 0;
     }
