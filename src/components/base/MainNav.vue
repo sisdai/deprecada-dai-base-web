4736eaba00771743d194034fecce03306a71f066
flkt <flkt.crnpio@gmail.com>
2021-10-28 12:54:27 -0500

    submenu

--- src/components/base/MainNav.vue
+++ src/components/base/MainNav.vue
@@ -19,15 +19,15 @@
         </a>
         <router-link class="main-nav-link" to="/">Inicio</router-link>
         <router-link class="main-nav-link" to="/guia-estilos">Guia de estilos</router-link>
-        <ul class="subnav">
-          <li>
-            <router-link class="main-nav-link" to="/">Menu</router-link>
-            <ul class="dropdown">
-              <li><router-link class="main-nav-link" to="/">Sublink 1</router-link></li>
-              <li><router-link class="main-nav-link" to="/">Sublink 2</router-link></li>
-            </ul>
-          </li>
-        </ul>
+
+        <div class="main-nav-submenu">
+          <span class="main-nav-link">Elemento con submenu</span>
+          <div class="submenu">
+            <router-link class="main-nav-link" to="/ayuda">Link en el submenu</router-link>
+            <router-link class="main-nav-link" to="/ayuda">Otro link con un texto larguisimo para probar que se pueda ir a varias lineas</router-link>
+          </div>
+        </div>
+
         <router-link class="main-nav-link" to="/ayuda">Ayuda</router-link>
       </div>
     </MainContainer>
@@ -53,6 +53,11 @@ export default {
     toggleMenu() {
       this.$store.commit('toggleMainNav');
     },
+    isMobile() {
+      let check = false;
+      (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+      return check;
+    },
   },
   computed: {
     hasMainNav() {
@@ -69,138 +74,120 @@ export default {
 </script>
 
 <style scoped lang="scss">
-ul.subnav li::before {
-  content: inherit;
-}
 .main-nav {
   background: map-get($color-gob, "verde");
   box-shadow: 0 0 10px rgba(0,0,0,.7);
 }
 .main-nav-top {
   background: map-get($color-gob, "verde");
+  box-shadow: 0 0 40px rgba(0,0,0,.2);
   position: relative;
-  margin-right: - ($gap);
-  margin-left: - ($gap);
+  margin-right: $gap * -1;
+  margin-left: $gap * -1;
   z-index: 2;
-  box-shadow: 0 0 20px rgba(0,0,0,.2);
 }
 .main-nav-menu {
   background: map-get($color-gob, "verde");
-  border-top: 1px solid rgba(255,255,255,.2);
-  display: flex;
-  flex-direction: column;
   overflow-y: auto;
   position: absolute;
   top: -100vh;
-  height: calc(100vh - 100px);
   height: calc((100 * var(--vh)) - 100px);
   width: 100%;
-  margin-right: - ($gap);
-  margin-left: - ($gap);
+  margin-right: $gap * -1;
+  margin-left: $gap * -1;
   transition: top .3s ease-in-out;
   z-index: 1;
   &.open {
     top: 50px;
   }
-  ul.subnav {
-    margin: 0 !important;
-    padding-left: 0 !important;
-    position: relative;
-    cursor: pointer;
-    li {
-      margin: 0 !important;      
-    }
-    ul {
-      margin: 0 !important;
-    }
-  }
-  ul.subnav li ul.dropdown {      
-    li a {
-      border: none;
-    }
-  }
-  @media (min-width: map-get($media-queries-limit, "navegacion")) {
-    position: relative;
-    overflow: inherit;
-    flex-direction: row;
-    height: auto;
-    top: 0px;
-    margin-right: - ($gap);
-    margin-left: - ($gap);
-    &.open {
-      top: 0;
-    }
-    // a:first-child {
-    //   margin-left: $gap*.5;
-    // }
-    a {
-      margin-left: $gap*.5;
-    }
-    ul.subnav ul {
-      margin: 0 !important;
-      padding-left: 0 !important;
-      position: relative;
-    }
-    ul.subnav li ul.dropdown { 
-      display:none;
-      li a {
-        border-bottom: 1px solid rgba(255,255,255,0.1);
-      }
-      li:hover{
-        background: map-get($color-gob, "verde-obscuro");
-        color: map-get($color-gob, "beige");
-      }
-    }
-    ul.subnav li:hover ul.dropdown {
-      background-color: map-get($color-gob, "verde-claro");
-      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-      display: block;
-      position: absolute;
-      left: 0px;
-      top: 50px;
-      z-index: 3;
-    }
-  }
+}
+.submenu {
+  background: map-get($color-gob, "verde-claro");
+  border-radius: 5px;
+  margin: 0 $gap*.5 $gap*.5;
 }
 .main-nav-external-link {
   font-size: map-get($fuente, "size-notas");
-  text-transform: uppercase;
-  display: flex;
-  padding: $gap*.5;
-  white-space: nowrap;
   color: map-get($color-gob, "dorado");
-  &:hover {
-      background: darken(map-get($color-gob, "verde"), 2%);
-      color: map-get($color-gob, "beige");
-    }
-    &:focus {
-      background: map-get($color-gob, "verde-obscuro");
-      color: map-get($color-gob, "beige");
-    }
+  text-transform: uppercase;
+  display: block;
+  padding: $gap*.5;
 }
 .main-nav-link {
   font-size: map-get($fuente, "size-secundario");
-  border-radius: 0;
   color: #fff;
-  display: flex;
+  display: block;
   padding: $gap*.5;
-  white-space: nowrap;
-  border-bottom: 1px solid rgba(255,255,255,0.1);
-  @media (min-width: map-get($media-queries-limit, "navegacion")) {
-    display: inline-flex;
-    // &:first-child {
-    //   margin-left: auto;
-    // }
-    // &:last-child {
-    //   margin-right: auto;
-    // }
-    &:hover {
-      background: darken(map-get($color-gob, "verde"), 2%);
-      color: map-get($color-gob, "beige");
+}
+.main-nav-external-link,
+.main-nav-link {
+  border-radius: 0;
+  display: block;
+  padding: $gap*.5;
+  line-height: 20px;
+  &:active,
+  &:focus,
+  &:hover {
+    background: darken(map-get($color-gob, "verde"), 2%);
+    color: map-get($color-gob, "beige");
+  }
+}
+
+@media (min-width: map-get($media-queries-limit, "navegacion")) {
+  .main-nav-menu {
+    position: relative;
+    overflow: inherit;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    height: inherit;
+    top: 0;
+    margin-right: $gap * -.5;
+    margin-left: $gap * -.5;
+    &.open {
+      top: 0;
     }
-    &:focus {
-      background: map-get($color-gob, "verde-obscuro");
-      color: map-get($color-gob, "beige");
+  }
+  .main-nav-external-link,
+  .main-nav-link {
+    display: inline-block;
+    white-space: nowrap;
+  }
+  
+  .main-nav-submenu {
+    position: relative;
+    .submenu {
+      margin: -5px 0 0;
+      opacity: 0;
+      position: absolute;
+      pointer-events: none;
+      transition: all .27s ease-in-out;
+      .main-nav-external-link,
+      .main-nav-link {
+        background: transparent;
+        border-bottom: 1px solid transparent;
+        margin: 0 $gap*.5;
+        padding-left: 0;
+        padding-right: 0;
+        display: block;
+        white-space: inherit;
+        &:active,
+        &:focus,
+        &:hover {
+          background: transparent;
+          border-bottom: 1px solid map-get($color-gob, "beige");
+          color: map-get($color-gob, "beige");
+        }
+      }
+    }
+    &:hover,
+    &:focus,
+    &:focus-within,
+    &:active {
+      .submenu {
+        opacity: 1;
+        pointer-events: auto;
+      }
     }
   }
 }
